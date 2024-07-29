b7c45c927865a9d158a358b2a373d45104e4ba13
Tejas Pisal <137098887+pisaltejas@users.noreply.github.com>
2024-07-29 09:40:50 +0000

    commit datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code  as newer version
+/*Alert is not defined because we are using nodejs, not browser*/
+// alert(3 + 3) 
+// avoid semicolon in JS
+console.log(3 + 3)
+console.log("Tejas")
+
+let age = 23
+let isloggedIN = false
+let state;
+//datatypes 
+//primitive datatypes
+//number => 2 to power 53
+//bigint
+//string = " "
+//boolean => true/false
+//null => standalone value 
+//undefined => 
+//symbol => unique
+
+//object
+//null is object
+console.log(typeof "Tejas") //string
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
